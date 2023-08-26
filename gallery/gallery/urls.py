c1c3dd12aa43f1a13f2947c42b194447e3627588
Pandora <nimfanefor@gmail.com>
2023-08-26 11:35:46 +0600

    Изменил название проекта

--- gallery/gallery/urls.py
+++ gallery/gallery/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for instagram project.
+URL configuration for gallery project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf.urls.static import static
 # from django.contrib import admin
 from django.urls import path, include
-from instagram import settings
+from gallery import settings
 
 api_urls = [
     path('v1/', include('api_v1.urls')),
